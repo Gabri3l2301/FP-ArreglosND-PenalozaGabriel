b2c67a27d73b7e953c7c11561ecadcb0cdf8d002
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-06 14:57:52 -0500

    feat: Añadir función para rellenar la matriz.

--- 002buscarValorMatriz.cpp
+++ 002buscarValorMatriz.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void rellenarMatriz(int matriz[][3]){
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            cout << "Ingrese el valor de la matriz [" << i << "][" << j << "]: ";
+            cin >> matriz[i][j];
+        }
+    }
+}
+
 int main (){
     
 }
