8734fa0a35db639c2c51f2717492b68a11e7a33b
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-06 15:04:52 -0500

    feat: Añadir función para buscar un valor en una matriz.

--- 002buscarValorMatriz.cpp
+++ 002buscarValorMatriz.cpp
@@ -10,6 +10,23 @@ void rellenarMatriz(int matriz[][3]){
     }
 }
 
+void buscarValorMatriz(int matriz[][3], int valor){
+    cout << "Ingresa el valor que desea buscar: ";
+    cin >> valor;
+    cout << "El valor " << valor << " se encuentra en:" << endl;
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            if (valor == matriz[i][j]){
+                cout << "Fila " << i << ", Columna " << j << endl;
+            }
+        }
+    }
+}
+
 int main (){
-    
+    int matriz[3][3];
+
+    rellenarMatriz(matriz);
+
+    return 0;
 }
