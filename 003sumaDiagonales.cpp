f2923adfee15ce483ba05ca1f9501fda066fb9f8
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-06 15:18:00 -0500

    feat: Agregar función para rellenar la matriz.

--- 003sumaDiagonales.cpp
+++ 003sumaDiagonales.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void rellenarMatriz(int matriz[][4]){
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            cout << "Ingrese el valor de la matriz [" << i << "][" << j << "]: ";
+            cin >> matriz[i][j];
+        }
+    }
+}
+
 int main (){
+    int matriz[4][4];
+
+    rellenarMatriz(matriz);
     return 0;
 }
