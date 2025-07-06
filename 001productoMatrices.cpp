f1a5efa2d1fc6c7b14d4e521455b69aa14548757
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-06 14:54:32 -0500

    001productoMatrices.cpp - version final

--- 001productoMatrices.cpp
+++ 001productoMatrices.cpp
@@ -10,12 +10,48 @@ void rellenarMatriz(int matriz[][3]){
     }
 }
 
+void productoMatriz(int matrizA[][3], int matrizB[][3], int producto[][3]){
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            for (int k = 0; k < 3; k++){
+                producto[i][j] += matrizA[i][k] * matrizB[k][j];
+            }
+        }
+    }
+}
+
+void imprimirMatrices(int matrizA[][3], int matrizB[][3], int producto[][3]){
+    cout << "\nMatriz 1\t\tMatriz 2\t\tProducto\n";
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            cout << matrizA[i][j] << " ";
+        }
+        cout << "\t\t\t";
+
+        for (int j = 0; j < 3; j++){
+            cout << matrizB[i][j] << " ";
+        }
+        cout << "\t\t\t";
+        
+        for (int j = 0; j < 3; j++){
+            cout << producto[i][j] << " ";
+        }
+        cout << endl;
+    } 
+}
+
 int main (){
     int matriz1[3][3];
     int matriz2[3][3];
-    int producto[3][3];
+    int producto[3][3] = {};
+
+    cout << "Datos de la matriz 1:" << endl;
     rellenarMatriz(matriz1);
+    cout << "Datos de la matriz 2:" << endl;
     rellenarMatriz(matriz2);
 
+    productoMatriz(matriz1, matriz2, producto);
+
+    imprimirMatrices(matriz1, matriz2, producto);
     return 0;
 }
