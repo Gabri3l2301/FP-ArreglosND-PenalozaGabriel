a778b2b98f44439b3521e53ddaaf94c01f7fe367
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-07 14:49:35 -0500

    feat: Agregar función para imprimir la espiral de la matriz.

--- 008espiralMatriz.cpp
+++ 008espiralMatriz.cpp
@@ -12,7 +12,40 @@ void rellenarMatriz(int matriz[][TAM], int filas, int columnas) {
     }
 }
 
-int main (){
+void imprimirEspiral(int matriz[][TAM], int n) {
+    int inicioFila = 0, finFila = n - 1;
+    int inicioCol = 0, finCol = n - 1;
 
+    cout << "Recorrido en espiral: ";
+    while (inicioFila <= finFila && inicioCol <= finCol) {
+        // Arriba izquierda a derecha
+        for (int j = inicioCol; j <= finCol; j++)
+            cout << matriz[inicioFila][j] << " ";
+        inicioFila++;
+
+        // Derecha arriba a abajo
+        for (int i = inicioFila; i <= finFila; i++)
+            cout << matriz[i][finCol] << " ";
+        finCol--;
+
+        // Abajo derecha a izquierda
+        if (inicioFila <= finFila) {
+            for (int j = finCol; j >= inicioCol; j--)
+                cout << matriz[finFila][j] << " ";
+            finFila--;
+        }
+
+        // Izquierda abajo a arriba
+        if (inicioCol <= finCol) {
+            for (int i = finFila; i >= inicioFila; i--)
+                cout << matriz[i][inicioCol] << " ";
+            inicioCol++;
+        }
+    }
+    cout << endl;
+}
+
+int main (){
+    
     return 0;
 }
