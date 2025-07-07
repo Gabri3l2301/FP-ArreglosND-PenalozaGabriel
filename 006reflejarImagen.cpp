ad957bcfc12c7b6079953c7b27adca6e1626710e
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-07 10:21:31 -0500

    feat: Agregar función para imprimir la matriz.

--- 006reflejarImagen.cpp
+++ 006reflejarImagen.cpp
@@ -12,7 +12,16 @@ void rellenarMatriz(int matriz[][TAM], int filas, int columnas) {
     }
 }
 
+void imprimirMatriz(int matriz[][TAM], int filas, int columnas) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            cout << matriz[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
 int main (){
-    
+
     return 0;
 }
