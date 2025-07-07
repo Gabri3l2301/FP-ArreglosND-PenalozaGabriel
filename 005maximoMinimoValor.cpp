dfe9dc0d4785779f11f49f56eda57560cc73fb66
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-07 09:55:08 -0500

    feat: Agregar función para encontrar el valor máximo y mínimo en una matriz.

--- 005maximoMinimoValor.cpp
+++ 005maximoMinimoValor.cpp
@@ -12,6 +12,28 @@ void rellenarMatriz(int matriz[][TAM], int filas, int columnas) {
     }
 }
 
+void encontrarMaxMin(int matriz[][TAM], int filas, int columnas) {
+    int max = matriz[0][0], min = matriz[0][0];
+    int maxFila = 0, maxCol = 0, minFila = 0, minCol = 0;
+
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            if (matriz[i][j] > max) {
+                max = matriz[i][j];
+                maxFila = i;
+                maxCol = j;
+            }
+            if (matriz[i][j] < min) {
+                min = matriz[i][j];
+                minFila = i;
+                minCol = j;
+            }
+        }
+    }
+    cout << "El valor máximo es " << max << " en la posición [" << maxFila << "][" << maxCol << "]" << endl;
+    cout << "El valor mínimo es " << min << " en la posición [" << minFila << "][" << minCol << "]" << endl;
+}
+
 int main (){
     
     return 0;
