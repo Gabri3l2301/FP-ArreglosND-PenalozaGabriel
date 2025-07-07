a62a4ee28a67f6a4e3960962d838734684d470fb
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-07 15:19:41 -0500

    Ejercicio 9 - versión final

--- 009.cpp
+++ 009.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void imprimirCuadradoLatino(int N) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            // La fórmula genera la rotación a la derecha
+            cout << ((j - i + N) % N + 1) << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main (){
-    
+    int N;
+    cout << "Ingrese el orden del cuadrado latino: ";
+    cin >> N;
+    imprimirCuadradoLatino(N);
+    return 0;
     return 0;
 }
