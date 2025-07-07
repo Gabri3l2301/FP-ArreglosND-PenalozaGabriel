0b6b52741ff7524bf339ea1e1b5feb99a672d7e8
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-07 14:15:53 -0500

    feat: Agregar función para pedir los datos de la matriz.

--- 008espiralMatriz.cpp
+++ 008espiralMatriz.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+const int TAM = 5;
+
+void rellenarMatriz(int matriz[][TAM], int filas, int columnas) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            cout << "Ingrese el valor de la matriz [" << i << "][" << j << "]: ";
+            cin >> matriz[i][j];
+        }
+    }
+}
+
 int main (){
 
     return 0;
