098fe2409a2de86c7f37d687aeba0f0bf0ba3d5e
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-07 15:30:46 -0500

    feat: Agregar función para generar el tablero.

--- 010buscaminas.cpp
+++ 010buscaminas.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+const int TAM = 10;
+
+void generarTablero(int tablero[][TAM], int filas, int columnas) {
+    srand(time(0));
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            tablero[i][j] = rand() % 2; // 1 = mina, 0 = vacío
+        }
+    }
+}
 
 int main (){
 
